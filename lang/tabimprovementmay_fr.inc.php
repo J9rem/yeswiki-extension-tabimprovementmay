a9430b26ecc4c0e114497e7bc991f926dfca046f
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2022-05-27 07:55:41 +0200

    doc(tableau): change hints for columns width

--- lang/tabimprovementmay_fr.inc.php
+++ lang/tabimprovementmay_fr.inc.php
@@ -7,8 +7,9 @@ return [
     
     // bazarliste.yaml
     "AB_bazartableau_defaultcolumnwidth_label" => "Largeur minimale des colonnes",
-    "AB_bazartableau_defaultcolumnwidth_hint" => "ex. : 100px ou 20% ; vide = largeur automatique",
+    "AB_bazartableau_defaultcolumnwidth_hint" => "ex. : 100px ou 20% ; vide = largeur automatique (vide est plus joli en cas de personnalisation par champ)",
     "AB_bazartableau_columnswidth_label" => "Largeur personnalisée des colonnes",
+    "AB_bazartableau_columnswidth_hint" => "ex. : 100px ou 20% ; vide = largeur automatique",
     "AB_bazartableau_columnswidth_field_label" => "Champ",
     "AB_bazartableau_columnswidth_width_label" => "Largeur",
     "AB_bazartableau_exportallcolumns_label" => "Exporter aussi les colonnes masquées",
